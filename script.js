0d71c134fc0a0b5931f308bcbe60466d7a0f8b38
farhanazizf <farhanazizf7@gmail.com>
2022-06-04 13:51:23 +0700

    add search input dan button

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 $("#homeMenu").on("click", function () {
-  alert("home");
+  // alert("home");
 
   $("#container-page-home").removeClass("d-none"); //muncul
   $("#container-page-search").addClass("d-none"); //hidden
 });
 
 $("#movieMenu").on("click", function () {
-  alert("movies");
-
   $("#container-page-search").removeClass("d-none"); //muncul
   $("#container-page-home").addClass("d-none"); //hidden
+});
+
+$("#button-search").on("click", function () {
+  // alert("lagi search");
+  alert($("#input-search").val());
 });
